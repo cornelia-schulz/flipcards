a6b0466f7625062b0dda76e5c5c67313a72ba535
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-02-03 21:29:34 +1100

    added new components for add and edit card

---
+++ src/components/Add.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+    export default {
+        name: 'Add'
+    }
+</script>
+
+<style></style>
--- src/components/Flipcard.vue
+++ src/components/Flipcard.vue
@@ -11,13 +11,18 @@
             <button @click="card--" :disabled="card <= 0">Previous</button>
             <button @click="card++" :disabled="card >= cards.length-1">Next</button>
         </div>
-    
+        <div class="menu">
+            <button class="menu-btn">Edit card</button>
+            <button class="menu-btn">Add new card</button>
+            <button class="menu-btn">Delete card</button>
+        </div>
     </div>
 </template>
 
 <script>
 import Front from './Front.vue'
 import Back from './Back.vue'
+import cards from './data.json'
 export default {
   name: 'Flipcard',
   props: {},
@@ -25,22 +30,7 @@ export default {
       return {
           front: true,
           card: 0,
-          cards: [
-        {
-            id: 1,
-            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqkayhT3NbL0s1YQqpLbhFnUSYNDqMMlZ1BPz0pi4H1PInC5r1AQ',
-            name: 'Hillary',
-            nickname: 'Hill',
-            url: 'https://en.wikipedia.org/wiki/Hillary_Clinton'
-        },
-        {
-            id: 2,
-            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvcY5983wXq-E0sMixhCetS8m0onB5GGpcw1nsowMgCBw5Rw1x',
-            name: 'Michelle',
-            nickname: 'Forever first lady',
-            url: 'https://becomingmichelleobama.com/'
-        }
-        ]
+          cards: cards.cards
       }
   },
   methods: {
@@ -81,5 +71,11 @@ export default {
         color: grey;
     }
 
-
+    .menu-btn {
+        background-color: black;
+        border: 1px solid #2c3e50;
+        height: 2em;
+        font-size: 1em;
+        margin-right: 5px;
+    }
 </style>
